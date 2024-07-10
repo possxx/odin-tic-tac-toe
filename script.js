ef11341818b984f33e6fee78ebf61a2002ef7b55
possxx <168581964+possxx@users.noreply.github.com>
2024-07-10 17:14:52 +0200

    Create IIFE for gameboard object

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameboard = (function () {
+    const row1 = [null, null, null];
+    const row2 = [null, null, null];
+    const row3 = [null, null, null];
+    const column1 = [null, null, null];
+    const column2 = [null, null, null];
+    const column3 = [null, null, null];
+    return { board: [row1, row2, row3, column1, column2, column3] };
+})();
+
