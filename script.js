6c53ac1a7eaad20430ca327679edb47537945e08
possxx <168581964+possxx@users.noreply.github.com>
2024-07-12 19:15:39 +0200

    Finish logic to play a game of tic tac toe

--- script.js
+++ script.js
@@ -8,13 +8,65 @@ function createPlayer(name, mark) {
     return { name, mark, getMarks, giveMarks, clearMarks };
 }
 
+const dom = (function() {
+    const playerNames = document.querySelector(".player-names");
+    const playerNamesButton = document.querySelector(".player-names button");
+    const domPlayer1 = document.querySelector(`.player-names input[name="name1"]`);
+    const domPlayer2 = document.querySelector(`.player-names input[name="name2"]`);
+    const playerTurn = document.querySelector(".player-turn");
+    const playerWinner = document.querySelector(".player-winner");
+    const startGameButton = document.querySelector(".start-game");
+    const newGameButton = document.querySelector(".new-game");
+    const row1Column1 = document.querySelector(".row1.column1");
+    const row1Column2 = document.querySelector(".row1.column2");
+    const row1Column3 = document.querySelector(".row1.column3");
+    const row2Column1 = document.querySelector(".row2.column1");
+    const row2Column2 = document.querySelector(".row2.column2");
+    const row2Column3 = document.querySelector(".row2.column3");
+    const row3Column1 = document.querySelector(".row3.column1");
+    const row3Column2 = document.querySelector(".row3.column2");
+    const row3Column3 = document.querySelector(".row3.column3");
+    const board = [row1Column1, row1Column2, row1Column3, row2Column1, row2Column2,
+                      row2Column3, row3Column1, row3Column2, row3Column3,
+                     ];
+
+    const updateNames = () => {
+        playerNamesButton.addEventListener("click", () => {
+            if (domPlayer1.value) {
+                player1.name = domPlayer1.value;
+                domPlayer1.setAttribute("placeholder", `${player1.name}`);
+                domPlayer1.value = "";
+            }
+            if (domPlayer2.value) {
+                player2.name = domPlayer2.value;
+                domPlayer2.setAttribute("placeholder", `${player2.name}`);
+                domPlayer2.value = "";
+            }
+        })
+    }
+
+    const newGame = () => {
+        startGameButton.addEventListener("click", () => {
+            startGameButton.classList.toggle("display-none");
+            playerNames.classList.toggle("display-none");
+            playerTurn.classList.toggle("display-none");
+            
+            game.playGame();
+        })
+    }
+
+    return { playerNames, domPlayer1, playerTurn, playerWinner, domPlayer2, startGameButton,
+             newGameButton, board, playerNamesButton, updateNames, newGame,
+           };
+})();
+
 const player1 = createPlayer("Player1", "X");
 const player2 = createPlayer("Player2", "O");
 
 const gameboard = (function () {
-    const row1 = [undefined, undefined, undefined];
-    const row2 = [undefined, undefined, undefined];
-    const row3 = [undefined, undefined, undefined];
+    const row1 = [dom.board[0], dom.board[1], dom.board[2]];
+    const row2 = [dom.board[3], dom.board[4], dom.board[5]];
+    const row3 = [dom.board[6], dom.board[7], dom.board[8]];
     return { board: [row1, row2, row3] };
 })();
 
@@ -44,83 +96,55 @@ const game = (function () {
     }
 
     const gameWin = () => {
-        if (gameboard.board[0][0] + gameboard.board[0][1] + gameboard.board[0][2] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[1][0] + gameboard.board[1][1] + gameboard.board[1][2] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[2][0] + gameboard.board[2][1] + gameboard.board[2][2] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[0][0] + gameboard.board[1][0] + gameboard.board[2][0] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[0][1] + gameboard.board[1][1] + gameboard.board[2][1] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[0][2] + gameboard.board[1][2] + gameboard.board[2][2] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[0][0] + gameboard.board[1][1] + gameboard.board[2][2] == player1.mark + player1.mark + player1.mark ||
-            gameboard.board[0][2] + gameboard.board[1][1] + gameboard.board[2][0] == player1.mark + player1.mark + player1.mark
+        if (gameboard.board[0][0].innerText + gameboard.board[0][1].innerText + gameboard.board[0][2].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[1][0].innerText + gameboard.board[1][1].innerText + gameboard.board[1][2].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[2][0].innerText + gameboard.board[2][1].innerText + gameboard.board[2][2].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[0][0].innerText + gameboard.board[1][0].innerText + gameboard.board[2][0].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[0][1].innerText + gameboard.board[1][1].innerText + gameboard.board[2][1].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[0][2].innerText + gameboard.board[1][2].innerText + gameboard.board[2][2].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[0][0].innerText + gameboard.board[1][1].innerText + gameboard.board[2][2].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark ||
+            gameboard.board[0][2].innerText + gameboard.board[1][1].innerText + gameboard.board[2][0].innerText == game.currentPlayerMark + game.currentPlayerMark + game.currentPlayerMark
         ) {
-            return `${player1.name} wins!`;
-        } else if (gameboard.board[0][0] + gameboard.board[0][1] + gameboard.board[0][2] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[1][0] + gameboard.board[1][1] + gameboard.board[1][2] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[2][0] + gameboard.board[2][1] + gameboard.board[2][2] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[0][0] + gameboard.board[1][0] + gameboard.board[2][0] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[0][1] + gameboard.board[1][1] + gameboard.board[2][1] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[0][2] + gameboard.board[1][2] + gameboard.board[2][2] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[0][0] + gameboard.board[1][1] + gameboard.board[2][2] == player2.mark + player2.mark + player2.mark ||
-            gameboard.board[0][2] + gameboard.board[1][1] + gameboard.board[2][0] == player2.mark + player2.mark + player2.mark
-                ) {
-                    return `${player2.name} wins!`;
+            return `${game.currentPlayer.name} wins!`;
         } else if (player1.getMarks() + player2.getMarks() == 9) {
             return "It's a tie!";
         }
     }
 
+    const currentPlayer = playerTurn();
+    const currentPlayerMark = playerMark();
+
     const playGame = () => {
-        let currentPlayer = game.playerTurn();
-        let currentPlayerMark = game.playerMark();
-        console.log( `It's your turn ${currentPlayer.name} (${currentPlayerMark})!`);
+        game.currentPlayer = playerTurn();
+        game.currentPlayerMark = playerMark();
+
+        dom.playerTurn.innerText = `It's your turn ${currentPlayer.name}`;
 
         function populateBoard() {
-            let currentPlayerTurn = prompt("Choose a row with a column like so: 11 (1 for Row, 1 for Column");
-            if (currentPlayerTurn == "11" && gameboard.board[0][0] == undefined) {
-                gameboard.board[0][0] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn == "12" && gameboard.board[0][1] == undefined) {
-                gameboard.board[0][1] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn == "13" && gameboard.board[0][2] == undefined) {
-                gameboard.board[0][2] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn == "21" && gameboard.board[1][0] == undefined) {
-                gameboard.board[1][0] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn == "22" && gameboard.board[1][1] == undefined) {
-                gameboard.board[1][1] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn == "23" && gameboard.board[1][2] == undefined) {
-                gameboard.board[1][2] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn == "31" && gameboard.board[2][0] == undefined) {
-                gameboard.board[2][0] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn = "32" && gameboard.board[2][1] == undefined) {
-                gameboard.board[2][1] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else if (currentPlayerTurn = "33" && gameboard.board[2][2] == undefined) {
-                gameboard.board[2][2] = currentPlayerMark;
-                currentPlayer.giveMarks();
-            } else {
-                populateBoard();
-            }
+            dom.board.forEach(item => {
+                item.addEventListener("click", () => {
+                    if (item.innerText == "" || item.innerText == undefined) {
+                        item.innerText = game.currentPlayerMark;
+                        game.currentPlayer.giveMarks();
+                        if (game.gameWin() == undefined) {
+                            game.currentPlayer = playerTurn();
+                            game.currentPlayerMark = playerMark();
+                            dom.playerTurn.innerText = `It's your turn ${game.currentPlayer.name}`;
+                        } else {
+                            dom.playerTurn.innerText = game.gameWin();
+                        }
+                    } else {
+                        dom.playerTurn.innerText = "Field is already occupied. Choose another one!";
+                    }
+                })
+            })
         }
 
         populateBoard();
-
-        if (game.gameWin() == undefined) {
-            game.playGame();
-        } else {
-            console.log( game.gameWin() );
-            player1.clearMarks();
-            player2.clearMarks();
-            gameboard.board[0] = [undefined, undefined, undefined];
-            gameboard.board[1] = [undefined, undefined, undefined];
-            gameboard.board[2] = [undefined, undefined, undefined];
-        }
     }
 
-    return { playerTurn, playerMark, gameWin, playGame };
+    return { playerTurn, playerMark, gameWin, playGame, currentPlayer, currentPlayerMark };
 })();
+
+dom.updateNames();
+dom.newGame();
